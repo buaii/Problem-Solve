a4cd4d843c14d0a88c2c3d70f815613a4ac63647
buaii <129252044+buaii@users.noreply.github.com>
2024-07-04 12:08:58 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,10 +1,17 @@
-def isPel(n):
-    return str(n) == str(n)[::-1]
-
-n = int(input())
-while n != 0:
-	if isPel(n):
-		print('yes')
+def isfel(s):
+	if len(s) <= 1:
+		return True
 	else:
-		print('no')
-	n = int(input())
+		if s[0] == s[-1]:
+			return isfel(s[1:-1])
+		else:
+			return False
+
+s = input()
+while s != "0":
+	if isfel(s):
+		print("yes")
+	else:
+		print("no")
+
+	s = input()
