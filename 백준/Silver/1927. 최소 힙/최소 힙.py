8d67dc5adf34cde5a4df5498730a4ebf5cd18a08
buaii <129252044+buaii@users.noreply.github.com>
2024-07-05 21:01:46 +0900

    [Silver II] Title: 최소 힙, Time: 132 ms, Memory: 37352 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+from collections import defaultdict
+import heapq
+
+n = int(sys.stdin.readline().rstrip())
+
+min_heap = []
+for i in range(n):
+	command = int(sys.stdin.readline().rstrip())
+	if command == 0:
+		if not min_heap:
+			sys.stdout.write("0\n")
+		else:
+			sys.stdout.write(f"{heapq.heappop(min_heap)}\n")
+	else:
+		heapq.heappush(min_heap, command)
