42b0a7f6f34a4faf8bddd49ea342de85dbb31d8e
buaii <129252044+buaii@users.noreply.github.com>
2024-07-06 10:35:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 34148 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+dp = [0] * 1001
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n+1):
+	dp[i] = (dp[i-1] + dp[i-2]) % 10007
+sys.stdout.write(f"{dp[n]}")
