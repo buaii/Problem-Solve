bc56a62f5825dc6fc08698e163d24285707d70af
buaii <129252044+buaii@users.noreply.github.com>
2024-07-08 21:32:06 +0900

    [Silver II] Title: 마인크래프트, Time: 128 ms, Memory: 34400 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,22 +1,20 @@
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
 n, m, b = map(int, input().strip().split())
 
+# 블록 높이 입력 받기
 graph = []
-min_height = 256
-max_height = 0
 for _ in range(n):
-    row = [int(x) for x in sys.stdin.readline().rstrip().split()]
-    graph.append(row)
-    min_height = min(min_height, min(row))
-    max_height = max(max_height, max(row))
+    graph.extend(map(int, input().rstrip().split()))
 
-# 높이별 블록 수를 미리 계산
-height_counts = [0] * 257
-for row in graph:
-    for height in row:
-        height_counts[height] += 1
+# 높이별 블록 수 계산
+height_counts = Counter(graph)
+
+min_height = min(height_counts)
+max_height = max(height_counts)
 
 time = sys.maxsize
 result = 0
@@ -24,11 +22,11 @@ result = 0
 for height in range(min_height, max_height + 1):
     use_block = 0
     get_block = 0
-    for current_height in range(257):
+    for current_height, count in height_counts.items():
         if current_height > height:
-            get_block += (current_height - height) * height_counts[current_height]
+            get_block += (current_height - height) * count
         else:
-            use_block += (height - current_height) * height_counts[current_height]
+            use_block += (height - current_height) * count
 
     if use_block > get_block + b:
         continue
@@ -38,4 +36,4 @@ for height in range(min_height, max_height + 1):
         time = count
         result = height
 
-sys.stdout.write(f"{time} {result}")
+sys.stdout.write(f"{time} {result}\n")
